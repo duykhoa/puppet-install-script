3c2a8cb5927c59f68ae2a543f2c2b80062b74fac
Kevin Tran <kevintran@ricoh.sg>
2015-05-02 23:34:44 +0800

    init my simple script

---
+++ manifests/site.pp
@@ -0,0 +1,87 @@
+Exec {
+    path => "/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
+}
+
+exec { "apt-get update": }
+
+package {
+  "git":
+    ensure => present
+}
+
+package {
+  "wget":
+    ensure => present
+}
+
+include rvm
+
+rvm_system_ruby {
+  'ruby-2.2.2':
+    ensure => 'present',
+    default_use => true;
+}
+
+class { 'locales':
+  locales => ['en_US.UTF-8 UTF-8']
+}
+
+package {
+  "postgresql":
+    ensure => present
+}
+
+package {
+  "postgresql-contrib":
+    ensure => present
+}
+
+package {
+  "openjdk-7-jre-headless":
+    ensure => true
+}
+
+package {
+  "libcurl4-openssl-dev":
+    ensure => present
+}
+
+package {
+  "nodejs":
+    ensure => true
+}
+
+package {
+  "imagemagick":
+}
+
+package {
+  "s3cmd":
+    ensure => true
+}
+
+package {
+  "ufw":
+    ensure => true
+}
+
+service {
+  "ufw":
+    ensure => running,
+    enable => true
+}
+
+exec { "ufw allow 22": }
+exec { "ufw allow 80": }
+exec { "ufw default deny": }
+
+package {
+  "passenger":
+    ensure   => present,
+    provider => "gem"
+}
+
+user { "deploy":
+  ensure   => present,
+  password => "5baa61e4c9b93f3f0682250b6cf8331b7ee68fd8" # SHA1 of "password"
+}
