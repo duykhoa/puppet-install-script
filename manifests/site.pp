7a299d6911d46da4acceaef8c9953639b8750529
Duy Khoa <duykhoa12t@gmail.com>
2015-05-04 11:49:35 +0800

    remove newrelic package (not available in normal mode)

--- manifests/site.pp
+++ manifests/site.pp
@@ -126,8 +126,3 @@ user { "deploy":
   ensure   => present,
   password => "5baa61e4c9b93f3f0682250b6cf8331b7ee68fd8" # SHA1 of "password"
 }
-
-package {
-  "newrelic-sysmond":
-    ensure   => present,
-}
